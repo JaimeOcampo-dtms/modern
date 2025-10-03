b85c327330aa7869010dc3d111e9c7bcd1c3313d
Manfred Steyer <manfred.steyer@gmx.net>
2025-10-03 23:39:31 +0200

    done

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -4,31 +4,62 @@ import {
   input,
   linkedSignal,
   numberAttribute,
+  WritableSignal,
 } from '@angular/core';
 
 import { FlightDetailStore } from '../flight-detail.store';
 import {
   form,
   required,
-  submit,
   schema,
   apply,
+  minLength,
+  maxLength,
+  Control,
 } from '@angular/forms/signals';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { debounceSignal } from '../../shared/debounce-signal';
 import { Flight, flightSchema } from '../../model/flight';
 import { toLocalDateTimeString } from '../../utils/date';
-import { AircraftComponent } from './aircraft/aircraft.component';
-import { PricesComponent } from './prices/prices.component';
-import { FlightComponent } from './flight/flight.component';
 import { ValidationErrorsComponent } from 'src/app/shared/validation-errors/validation-errors.component';
+import { FieldMetaDataComponent } from 'src/app/shared/field-meta-data/field-meta-data.component';
 
 export const flightFormSchema = schema<Flight>((path) => {
   apply(path, flightSchema);
   required(path.id);
 });
+
+export interface FieldDef {
+  name: string;
+  label: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  type?: string;
+}
+
+function toSchema(meta: FieldDef[]) {
+  return schema<unknown>((path) => {
+    for (const fieldDef of meta) {
+      const prop = fieldDef.name;
+      const fieldPath = (path as any)[prop];
+
+      if (!fieldPath) {
+        continue;
+      }
+      if (fieldDef.required) {
+        required(fieldPath);
+      }
+      if (typeof fieldDef.minLength !== 'undefined') {
+        minLength(fieldPath, fieldDef.minLength);
+      }
+      if (typeof fieldDef.maxLength !== 'undefined') {
+        maxLength(fieldPath, fieldDef.maxLength);
+      }
+    }
+  });
+}
 
 @Component({
   selector: 'app-flight-edit',
@@ -36,10 +67,8 @@ export const flightFormSchema = schema<Flight>((path) => {
     MatDatepickerModule,
     MatInputModule,
     MatProgressSpinnerModule,
-    AircraftComponent,
-    PricesComponent,
-    FlightComponent,
     ValidationErrorsComponent,
+    Control,
   ],
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
@@ -47,32 +76,38 @@ export const flightFormSchema = schema<Flight>((path) => {
 export class FlightEditComponent {
   private store = inject(FlightDetailStore);
 
+  flight = linkedSignal(() => normalize(this.store.flightValue()));
+  error = this.store.flightError;
+
   id = input.required({
     transform: numberAttribute,
   });
 
-  isPending = debounceSignal(this.store.saveFlightIsPending, 300);
-  error = this.store.saveFlightError;
+  meta: FieldDef[] = [
+    { name: 'id', label: 'Id', required: true },
+    {
+      name: 'from',
+      label: 'From',
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    { name: 'to', label: 'To', required: true, minLength: 3, maxLength: 20 },
+    { name: 'date', label: 'Date', required: true, type: 'datetime-local' },
+    { name: 'delayed', label: 'Delayed', type: 'checkbox' },
+  ];
 
-  flight = linkedSignal(() => normalize(this.store.flightValue()));
-  flightForm = form(this.flight, flightSchema);
+  // Let's assume we don't know the structure of the entity
+  // upfront but we have fitting meta data
+  entity = this.flight as WritableSignal<unknown>;
+  dynamicForm = form(this.entity, toSchema(this.meta));
 
   constructor() {
     this.store.updateFilter(this.id);
   }
 
   save(): void {
-    submit(this.flightForm, async (form) => {
-      const result = await this.store.saveFlight(form().value());
-
-      if (result.status === 'error') {
-        return {
-          kind: 'processing_error',
-          error: result.error,
-        };
-      }
-      return null;
-    });
+    console.log('save', this.dynamicForm().value());
   }
 }
 
