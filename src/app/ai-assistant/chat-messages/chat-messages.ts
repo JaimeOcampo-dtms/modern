b2f053b74a1ba9a4a6062a107f54bf2327c8a5c9
Manfred Steyer <manfred.steyer@gmx.net>
2026-02-05 11:07:42 +0100

    feat: update for article

--- src/app/ai-assistant/chat-messages/chat-messages.ts
+++ src/app/ai-assistant/chat-messages/chat-messages.ts
@@ -3,8 +3,6 @@ import { Component, computed, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-import { MarkdownComponent } from 'ngx-markdown';
-
 import { AnyTool } from 'node_modules/@hashbrownai/core/src/models/view.models';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { ToolStatusComponent } from '../assistant-chat/tool-status/tool-status';
