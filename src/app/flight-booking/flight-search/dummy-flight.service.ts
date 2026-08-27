d9ece14001408d4a832ef4806a25a0c8d0327902
Jaime Ocampo <jaime.ocampo@dtms.de>
2026-08-27 09:55:34 +0000

    refactor: simplify DummyFlightService by extending FlightService and removing unused properties

--- src/app/flight-booking/flight-search/dummy-flight.service.ts
+++ src/app/flight-booking/flight-search/dummy-flight.service.ts
@@ -2,20 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Flight } from '../../model/flight';
 import { FlightService } from './flight.service';
-import { Price } from 'src/app/model/price';
-import { initAircraft } from 'src/app/model/aircraft';
 
 @Injectable()
-export class DummyFlightService implements FlightService {
-  find(from: string, to: string): Observable<Flight[]> {
+export class DummyFlightService extends FlightService {
+  override find(from: string, to: string): Observable<Flight[]> {
     const date = new Date().toISOString();
-    const prices: Price[] = [];
-    const aircraft = initAircraft;
 
     return of([
-      { id: 7, from: 'here', to: 'there', date, delayed: false, aircraft, prices },
-      { id: 8, from: 'here', to: 'there', date, delayed: false, aircraft, prices },
-      { id: 9, from: 'here', to: 'there', date, delayed: false, aircraft, prices },
+      { id: 7, from, to, date, delayed: false, delay: 0 },
+      { id: 8, from, to, date, delayed: false, delay: 0 },
+      { id: 9, from, to, date, delayed: false, delay: 0 },
     ]);
   }
 }
